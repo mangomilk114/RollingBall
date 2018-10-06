9fd1b4ab1882a0f42c0b38192e20c30a5c25b222
kdh3040 <kdh30400@naver.com>
2018-10-06 21:55:30 +0900

    [김도형] 애드몹 작업중

---
+++ RollingBall/Assets/Script/Admanager.cs
@@ -0,0 +1,62 @@
+﻿using GoogleMobileAds.Api;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Admanager : MonoBehaviour {
+
+    public static Admanager _instance = null;
+    public static Admanager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<Admanager>() as Admanager;
+            }
+            return _instance;
+        }
+    }
+
+    //public string android_banner_id = "ca-app-pub-1646746921531266/5192364858";
+    public string android_banner_id = "ca-app-pub-3940256099942544/6300978111";
+    
+    public string ios_banner_id = "ca-app-pub-1646746921531266/5930731456";
+
+    private BannerView bannerView;
+
+
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(this);
+        RequestBannerAd();
+    }
+
+    public void RequestBannerAd()
+    {
+        string adUnitId = string.Empty;
+
+#if UNITY_ANDROID
+        adUnitId = android_banner_id;
+#elif UNITY_IOS
+        adUnitId = ios_bannerAdUnitId;
+#endif
+
+
+        bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);
+        AdRequest request = new AdRequest.Builder().Build();
+
+        bannerView.LoadAd(request);
+        bannerView.Show();
+    }
+
+    public void ShowBannerAd()
+    {
+      
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
